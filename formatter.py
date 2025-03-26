080083aa2d943653b44ec2ceea049813d7a153ca
saeram00 <saeram00.nova@gmail.com>
2025-03-26 16:09:32 -0300

    Compute results functionality
    feat: added compute_results function to calculate the sum of the scores
    from the test
    feat: deleted ScoreCalculator class, since compute_results was the
    proper implementation in this case

--- formatter.py
+++ formatter.py
@@ -2,11 +2,7 @@ import csv
 import json
 
 
-DFLT_FIELDNAMES: tuple[str, ...] = (
-    "question_number",
-    "item_factor",
-    "answer"
-)
+DFLT_FIELDNAMES: tuple[str, ...] = ("question_number", "item_factor", "answer")
 
 
 class AnswerFormatter(csv.DictWriter):
@@ -27,26 +23,14 @@ class AnswerFormatter(csv.DictWriter):
         for index, answer in enumerate(answers, start=1):
             for factor, question_numbers in self.__i_factors.items():
                 if index in question_numbers:
-                    fields.update({
-                        "question_number": index,
-                        "item_factor": factor,
-                        "answer": answer
-                    })
+                    fields.update(
+                        {
+                            "question_number": index,
+                            "item_factor": factor,
+                            "answer": answer,
+                        }
+                    )
                     self.writerow(fields)
-
-
-class ScoreCalculator(csv.DictReader):
-    """
-    Custom class meant to compute the results from the test.
-    """
-
-    def __init__(self, file, fieldnames=DFLT_FIELDNAMES, dialect="excel"):
-        super().__init__(file, fieldnames=fieldnames, dialect=dialect)
-        with open("item_factors.json") as jsonfile:
-            self.__i_factors = json.load(jsonfile)["factors"]
-
-    def compute_answers(self) -> None:
-        field_dict: dict = dict().fromkeys(self.fieldnames)
 
 
 def answer_input() -> list[int]:
@@ -63,3 +47,17 @@ def answer_input() -> list[int]:
         except ValueError:
             print("Must input a number between 1-6:")
     return answers
+
+
+def compute_results(file: str) -> dict:
+    results: dict = {
+        "low_selfesteem": 0,
+        "conflict_resolution": 0,
+        "expressiveness": 0,
+        "self_sufficiency": 0,
+    }
+    with open(file, "r", encoding="utf-8", newline="") as user_file:
+        scores = csv.DictReader(user_file, dialect="excel")
+        for score in scores:
+            results[score["item_factor"]] += int(score["answer"])
+    return results
--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import formatter as ft
 def main() -> int:
     if len(sys.argv) > 2:
         sys.exit("Must invoke with 0 or 1 arguments [user_name].")
-    user_name: str = sys.argv[1] if len(sys.argv) == 2 else input(
-        "Enter the name of the user (enter 'q' to quit): "
+    user_name: str = (
+        sys.argv[1]
+        if len(sys.argv) == 2
+        else input("Enter the name of the user (enter 'q' to quit): ")
     )
     while not user_name.isalpha():
         print("Must enter a name:")
@@ -18,9 +20,8 @@ def main() -> int:
     with open(f"{user_name}.csv", "w", encoding="utf-8", newline="") as new_f:
         t_formatter = ft.AnswerFormatter(new_f)
         t_formatter.write_answers(ft.answer_input())
-    with open(f"{user_name}.csv", "r", encoding="utf-8", newline="") as p_file:
-        t_calc = ft.ScoreCalculator(p_file)
-        t_calc.compute_answers()
+    results: dict = ft.compute_results(f"{user_name}.csv")
+    print(results)
     sys.exit(0)
 
 
